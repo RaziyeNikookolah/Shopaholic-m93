62e70ac20f999703f7cbc81d9e08dca329bd1850
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 13:11:22 +0330

    feat:add signal for slug in category

--- ShoesOnlineStore/shoes/signals.py
+++ ShoesOnlineStore/shoes/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils.text import slugify
 
-from .models import Product
+from .models import Product, Category
 
 
 @receiver(pre_save, sender=Product)
 def create_product(sender, instance, *args, **kwargs):
     if not instance.slug:
-        instance.slug = create_unique_slug(instance)
+        instance.slug = create_unique_slug_for_product(instance)
 
 
-def create_unique_slug(instance, new_slug=None):
+def create_unique_slug_for_product(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
@@ -22,5 +22,26 @@ def create_unique_slug(instance, new_slug=None):
 
     if qs.exists():
         new_slug = f"{slug}-{qs.first().id}"
-        return create_unique_slug(instance, new_slug)
+        return create_unique_slug_for_product(instance, new_slug)
+    return slug
+
+
+@receiver(pre_save, sender=Category)
+def create_category(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_unique_slug_for_category(instance)
+
+
+def create_unique_slug_for_category(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title, allow_unicode=True)
+
+    instanceClass = instance.__class__
+    qs = instanceClass.objects.filter(slug=slug)
+
+    if qs.exists():
+        new_slug = f"{slug}-{qs.first().id}"
+        return create_unique_slug_for_category(instance, new_slug)
     return slug
