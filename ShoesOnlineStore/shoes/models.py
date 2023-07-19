6b16bbcb98f4edd4302ac6c808f015ba460d8a4c
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 06:08:28 +0330

    feat:base structure of all models in shoes app

--- ShoesOnlineStore/shoes/models.py
+++ ShoesOnlineStore/shoes/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from core.models import BaseModel
+
+
+class Product(BaseModel):
+    ...
+
+
+class ProductSize(BaseModel):
+    ...
+
+
+class ProductImage(BaseModel):
+    ...
+
+
+class Category(BaseModel):
+    ...
