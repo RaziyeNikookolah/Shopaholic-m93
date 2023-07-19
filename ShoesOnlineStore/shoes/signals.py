ad888237db2f27f4a9492e78c4473644cd6df25f
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 07:58:52 +0330

    impl slug receiver

--- ShoesOnlineStore/shoes/signals.py
+++ ShoesOnlineStore/shoes/signals.py
@@ -1,2 +1,26 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.utils.text import slugify
+
+from .models import Product
+
+
+@receiver(pre_save, sender=Product)
+def create_product(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_unique_slug(instance)
+
+
+def create_unique_slug(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.brand, allow_unicode=True)
+
+    instanceClass = instance.__class__
+    qs = instanceClass.objects.filter(slug=slug)
+
+    if qs.exists():
+        new_slug = f"{slug}-{qs.first().id}"
+        return create_unique_slug(instance, new_slug)
+    return slug
