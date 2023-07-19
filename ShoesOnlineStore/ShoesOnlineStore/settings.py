3f85f54c896122a1ccdb5a7f19b4b294471cd7d6
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 19:31:14 +0330

    feat:chng bd engin

--- ShoesOnlineStore/ShoesOnlineStore/settings.py
+++ ShoesOnlineStore/ShoesOnlineStore/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'ShoesOnlineStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
