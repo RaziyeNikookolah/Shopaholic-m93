2131b49c220ddf39c89429177e8bfec60098d4fb
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 12:27:16 +0330

    fix:modify admin.py

--- ShoesOnlineStore/shoes/admin.py
+++ ShoesOnlineStore/shoes/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Product)
-admin.site.register(models.ProductSize)
-admin.site.register(models.ProductImage)
+admin.site.register(models.Size)
+admin.site.register(models.Image)
+admin.site.register(models.Color)
 admin.site.register(models.Category)
