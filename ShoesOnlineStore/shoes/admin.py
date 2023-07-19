c5a67d446c4d829e58e6acadd03578cac16cd3df
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 08:21:13 +0330

    chore:register shoes models to admin.py

--- ShoesOnlineStore/shoes/admin.py
+++ ShoesOnlineStore/shoes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.ProductSize)
+admin.site.register(models.ProductImage)
+admin.site.register(models.Category)
