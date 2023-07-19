e1e4f0f3a8ed271bdd0abf49d2a55ebe987254e7
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-19 06:20:29 +0330

    feat:impl Product model

--- ShoesOnlineStore/shoes/models.py
+++ ShoesOnlineStore/shoes/models.py
@@ -3,7 +3,13 @@ from core.models import BaseModel
 
 
 class Product(BaseModel):
-    ...
+    code = models.CharField(max_length=10)
+    brand = models.CharField(max_length=150)
+    manufacturing_country = models.CharField(200)
+    descriptions = models.TextField(max_length=250, null=True, blank=True)
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="products")
+    is_active = models.BooleanField(default=True, blank=True)
 
 
 class ProductSize(BaseModel):
