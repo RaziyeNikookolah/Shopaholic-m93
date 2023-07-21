5296fc241b173d99f9b3d185a6fe936f43cab30b
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-21 07:19:06 +0330

    feat:impl send_otp_code function

---
+++ ShoesOnlineStore/core/utils.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from kavenegar import *
+
+
+def send_otp_code(phone_number, code):
+    try:
+     # TODO env
+        api = KavenegarAPI(
+            '563470796A30362B74617A5167474C4A614F774970523458474632726E724E63343852434A34586C5A44593D')
+        params = {
+            'sender': '',  # if i pay money  I 'll get special phone_number
+            'receptor': phone_number,
+            'message': f'{code}  کد تایید شما '
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
