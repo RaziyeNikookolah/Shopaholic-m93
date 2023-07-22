2a48f7c493ffd834df695caa8fab619b2ae9d757
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-22 17:03:30 +0330

    chore:add iran provinces data

--- ShoesOnlineStore/accounts/models.py
+++ ShoesOnlineStore/accounts/models.py
@@ -93,6 +93,40 @@ class Address(BaseModel):
     address = models.TextField(_("adderess"), max_length=100)
     postal_code = models.CharField(_("postal code"), max_length=20)
 
+    PROVINCES = [
+        ('thr', _('Tehran')),
+        ('az-sh', _('Azarbayejan Sharghi')),
+        ('az-gh', _('Azarbayejan Gharbi')),
+        ('ard', _('Ardebil')),
+        ('esf', _('Esfehan')),
+        ('alb', _('Alborz')),
+        ('ilm', _('Ilam')),
+        ('bsh', _('Booshehr')),
+        ('chm', _('Charmahal o Bakhtiari')),
+        ('kh-j', _('Khorasan Jonobi')),
+        ('kh-r', _('Khorasan Razavi')),
+        ('kh-sh', _('Khorasan Shomali')),
+        ('khz', _('Khoozestan')),
+        ('znj', _('Zanjan')),
+        ('smn', _('Semnan')),
+        ('sbch', _('Sistan Baloochestan')),
+        ('frs', _('Fars')),
+        ('ghz', _('Ghazvin')),
+        ('qom', _('Qom')),
+        ('krd', _('Kordestan')),
+        ('krm', _('Kerman')),
+        ('kr-sh', _('Kerman Shah')),
+        ('khb', _('Kohkilooye Boyer Ahmad')),
+        ('gls', _('Golestan')),
+        ('gil', _('Gilan')),
+        ('lor', _('Lorestan')),
+        ('maz', _('Mazandaran')),
+        ('mrk', _('Markazi')),
+        ('hrm', _('Hormozgan')),
+        ('hmd', _('Hamedan')),
+        ('yzd', _('Yazd')),
+    ]
+
     class Meta:
         verbose_name = _("address")
         verbose_name_plural = _('adresses')
