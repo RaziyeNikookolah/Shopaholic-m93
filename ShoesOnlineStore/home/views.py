fe8f50b88d7effec507719268fc8caf9bebc0ea9
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-30 02:38:55 -0700

    feat:impl home view

--- ShoesOnlineStore/home/views.py
+++ ShoesOnlineStore/home/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
+# from menu_items.models import MenuItem, Category
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        # menu = MenuItem.objects.all()
+        # categories = Category.objects.all()
+        # info = RestaurantInfo.objects.first()
+        return render(
+            request,
+            "index.html",
+            # context={"menu": menu, "categories": categories, "info": info},
+        )
+
+
+class AboutView(View):
+    def get(self, request):
+        # info = RestaurantInfo.objects.first()
+        return render(request, "about_page.html"
+                      #   , context={"info": info}
+                      )
