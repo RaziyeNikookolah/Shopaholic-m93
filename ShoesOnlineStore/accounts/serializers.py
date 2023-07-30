1ae4365fe0786229a37ee194133b19dc3429b0b4
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-30 01:09:39 -0700

    chore:move jwt serializer to account serializer

--- ShoesOnlineStore/accounts/serializers.py
+++ ShoesOnlineStore/accounts/serializers.py
@@ -1,5 +1,4 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
@@ -9,12 +8,3 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
         token['phone_number'] = user.phone_number
         return token
-
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/token',
-        '/token/refresh',
-    ]
-    return Response(routes)
