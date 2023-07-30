11336160f6290d06f0e1cf8a5b959199f80327f6
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-29 23:03:44 -0700

    feat : add jwt auth to setting

--- ShoesOnlineStore/ShoesOnlineStore/settings.py
+++ ShoesOnlineStore/ShoesOnlineStore/settings.py
@@ -94,11 +94,14 @@ DATABASES = {
     }
 }
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # it makes defualt for all methods
