3335d41ad2131c0ecd1332e5db76996f915c670d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-29 23:07:43 -0700

    chore:add smpl jwt to apps

--- ShoesOnlineStore/ShoesOnlineStore/settings.py
+++ ShoesOnlineStore/ShoesOnlineStore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt.token_blacklist',
     'home.apps.HomeConfig',
     'core.apps.CoreConfig',
     'orders.apps.OrdersConfig',
