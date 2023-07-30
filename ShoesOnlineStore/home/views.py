f385059dc4dbca8467e9baade11626f9747fa520
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-30 03:46:31 -0700

    chore : load static element in index.html

--- ShoesOnlineStore/home/views.py
+++ ShoesOnlineStore/home/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-# from menu_items.models import MenuItem, Category
 
 
 class HomeView(View):
     def get(self, request):
-        # menu = MenuItem.objects.all()
-        # categories = Category.objects.all()
-        # info = RestaurantInfo.objects.first()
+
         return render(
             request,
             "index.html",
-            # context={"menu": menu, "categories": categories, "info": info},
         )
 
 
 class AboutView(View):
     def get(self, request):
-        # info = RestaurantInfo.objects.first()
         return render(request, "about_page.html"
-                      #   , context={"info": info}
                       )
