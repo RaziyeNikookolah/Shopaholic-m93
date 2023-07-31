94316a989b2916d6e5e7f9637ea95f4737b6292a
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-31 08:08:14 -0700

    chore:manage forms

--- ShoesOnlineStore/statics/js/script.js
+++ ShoesOnlineStore/statics/js/script.js
@@ -4,28 +4,69 @@
 
 $(document).ready(function () {
     // Hide the H2 element initially
-    $('#div-login').hide();
+    $('#signup-div', '#code-verify-div').hide();
 
     // Listen for click event on the button
     $('#li_login').on('click', function () {
         // Toggle the H2 element's visibility on button click
-        $('#div-login').toggle();
+        $('#signup-div').toggle();
     });
 });
-
-$('#login_btn').on('click', function () {
-
-    let cookie = document.cookie
-    let csrfToken = cookie.substring(cookie.indexOf('=') + 1)
+phone_number2 = $('#phone_number2')
+// $('#login_btn').on('click', function () {
+$(document).on('submit', '#login_form', function (e) {
+    e.preventDefault();
+    // let cookie = document.cookie
+    // let csrfToken = cookie.substring(cookie.indexOf('=') + 1)
 
     $.ajax({
-        url: 'accounts/get_phone_number/',
+
+        url: 'otp/request_otp/',
         type: 'POST',
-        headers: {
-            'X-CSRFToken': csrfToken
-        }
+        // headers: {
+        //     'X-CSRFToken': csrfToken
+        // }
+        data: {
+            phone_number: $('#phone_number').val(),
+            'csrfmiddlewaretoken': '{{csrf_token}}'
+        },
+        dataType: "json"
+
+        // csrfmiddleware_token: $('input[name=csrfmiddleware_token]').val()
     }).then(res => {
 
         console.log(res);
     });
+    $('#signup-div').hide();
+    $('#code-verify-div').show();
+    phone_number2.val($('#phone_number').val())
+
+});
+$(document).on('submit', '#code-verify_form', function (e) {
+    e.preventDefault();
+    // let cookie = document.cookie
+    // let csrfToken = cookie.substring(cookie.indexOf('=') + 1)
+
+    $.ajax({
+
+        url: 'otp/verify_otp/',
+        type: 'POST',
+        // headers: {
+        //     'X-CSRFToken': csrfToken
+        // }
+        data: {
+            phone_number: $('#phone_number2').val(),
+            code: $('#code').val(),
+            'csrfmiddlewaretoken': '{{csrf_token}}'
+        },
+        dataType: "json"
+
+        // csrfmiddleware_token: $('input[name=csrfmiddleware_token]').val()
+    }).then(res => {
+
+        console.log(res);
+    });
+    $('#signup-div').hide();
+    $('#code-verify-div').hide();
+
 });
