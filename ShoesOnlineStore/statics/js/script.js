a0428c8490ee7a02de43f92056a848a942155dff
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-30 23:53:13 -0700

    feat: scrip for login

--- ShoesOnlineStore/statics/js/script.js
+++ ShoesOnlineStore/statics/js/script.js
@@ -1,3 +1,14 @@
-function login() {
-    $("#div-login").slideUp();
-}
+
+
+
+
+$(document).ready(function () {
+    // Hide the H2 element initially
+    $('#div-login').hide();
+
+    // Listen for click event on the button
+    $('#li_login').on('click', function () {
+        // Toggle the H2 element's visibility on button click
+        $('#div-login').toggle();
+    });
+});
