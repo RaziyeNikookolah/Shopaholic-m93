2db17fcacfabd99b9398568687ee035b79e90ec0
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-31 19:07:19 -0700

    chore:no need serializer for this time->comment it

--- ShoesOnlineStore/accounts/serializers.py
+++ ShoesOnlineStore/accounts/serializers.py
@@ -1,10 +1,10 @@
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['phone_number'] = user.phone_number
-        return token
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#         token['phone_number'] = user.phone_number
+#         return token
