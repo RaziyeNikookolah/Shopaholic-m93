cb9d1d7642e9203b246c0d1e5c8976a5cf737052
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-01 00:24:28 -0700

    chore:del rest_framework.authtoken from instlldapp

--- ShoesOnlineStore/ShoesOnlineStore/settings.py
+++ ShoesOnlineStore/ShoesOnlineStore/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'rest_framework_simplejwt',
