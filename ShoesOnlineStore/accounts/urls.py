e01d5d1b4f3f9ceb4cf6fc01650ea657ab061606
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-31 19:07:56 -0700

    chore:put token url in main url

--- ShoesOnlineStore/accounts/urls.py
+++ ShoesOnlineStore/accounts/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
-    path('', views.getRoutes, name='routes'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('get_phone_number/', views.get_phon_number, name="get_phone_number")
+    # path('', views.getRoutes, name='routes'),
+
 
 ]
