f08f6da9262b351448bf8c34db2aebf5051322f2
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-04 04:21:54 +0330

    chore:mk shop_single to hv req data

---
+++ ShoesOnlineStore/statics/js/shop_single.js
@@ -0,0 +1,146 @@
+// $(document).ready(function () {
+//     let searchParams = new URLSearchParams(window.location.search)
+//     console.log(searchParams);
+
+//     searchParams.has('pk') // true
+
+//     let param = searchParams.get('pk')
+//     $.ajax({
+//         url: '',
+//         type: 'GET',
+//         dataType: "json",
+//         data: { pk: param },
+//         success: function (data, status, xhr) {
+//             console.log(data);
+//             output = '<div class="site-section">' +
+//                 '<div class="container">' +
+//                 '<div class="row">' +
+//                 '<div class="col-md-6">' +
+//                 '<img src="{{ product.image }}" alt="Image" class="img-fluid">' +
+//                 '</div>' +
+//                 '<div class="col-md-6">' +
+//                 '<h2 class="text-black">Tank Top T-Shirt</h2>' +
+//                 '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, vitae, explicabo? Incidunt facere, natus soluta dolores iusto! Molestiae expedita veritatis nesciunt doloremque sint asperiores fuga voluptas, distinctio, aperiam, ratione dolore.</p>' +
+//                 '<p class="mb-4">Ex numquam veritatis debitis minima quo error quam eos dolorum quidem perferendis. Quos repellat dignissimos minus, eveniet nam voluptatibus molestias omnis reiciendis perspiciatis illum hic magni iste, velit aperiam quis.</p>' +
+//                 '<p><strong class="text-primary h4">$50.00</strong></p>' +
+//                 '<div class="mb-1 d-flex">' +
+//                 '<label for="option-sm" class="d-flex mr-3 mb-3">' +
+//                 '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-sm" name="shop-sizes"></span> <span class="d-inline-block text-black">Small</span>' +
+//                 '</label>' +
+//                 '<label for="option-md" class="d-flex mr-3 mb-3">' +
+//                 '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-md" name="shop-sizes"></span> <span class="d-inline-block text-black">Medium</span>' +
+//                 '</label>' +
+//                 '<label for="option-lg" class="d-flex mr-3 mb-3">' +
+//                 '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-lg" name="shop-sizes"></span> <span class="d-inline-block text-black">Large</span>' +
+//                 '</label>' +
+//                 '<label for="option-xl" class="d-flex mr-3 mb-3">' +
+//                 '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-xl" name="shop-sizes"></span> <span class="d-inline-block text-black"> Extra Large</span>' +
+//                 '</label>' +
+//                 '</div>' +
+//                 '<div class="mb-5">' +
+//                 '<div class="input-group mb-3" style="max-width: 120px;">' +
+//                 '<div class="input-group-prepend">' +
+//                 '<button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>' +
+//                 '</div>' +
+//                 '<input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">' +
+//                 '<div class="input-group-append">' +
+//                 '<button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '{% load static %}' +
+
+//                 '</div>' +
+//                 '<p><a href="" class="buy-now btn btn-sm btn-primary">Add To Cart</a></p>' +
+
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>';
+//             $('#product_details').html(output);
+
+
+//         },
+//         error: function (jqXhr, textStatus, errorMessage) { // error callback
+//             console.log('Error in loading data', errorMessage);
+//         },
+//     });
+
+
+// });
+
+
+
+// // fetch('https://127.0.0.1:8000/shoes/shop_single')
+// //     .then(function (response) {
+// //         if (response.ok) {
+// //             return response.json();
+// //         }
+// //         throw new Error('Network response was not OK.');
+// //     })
+// //     .then(function (data) {
+// //         var fieldValue = data.fieldName; // Access the field "fieldName" from the response object
+// //         console.log(fieldValue);
+
+
+
+
+
+
+
+// //         output = '<div class="site-section">' +
+// //             '<div class="container">' +
+// //             '<div class="row">' +
+// //             '<div class="col-md-6">' +
+// //             '<img src="' + data.image + '" alt="Image" class="img-fluid">' +
+// //             '</div>' +
+// //             '<div class="col-md-6">' +
+// //             '<h2 class="text-black">' + data.brand + '</h2>' +
+// //             '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, vitae, explicabo? Incidunt facere, natus soluta dolores iusto! Molestiae expedita veritatis nesciunt doloremque sint asperiores fuga voluptas, distinctio, aperiam, ratione dolore.</p>' +
+// //             '<p class="mb-4">Ex numquam veritatis debitis minima quo error quam eos dolorum quidem perferendis. Quos repellat dignissimos minus, eveniet nam voluptatibus molestias omnis reiciendis perspiciatis illum hic magni iste, velit aperiam quis.</p>' +
+// //             '<p><strong class="text-primary h4">' + data.price + '</strong></p>' +
+// //             '<div class="mb-1 d-flex">' +
+// //             '<label for="option-sm" class="d-flex mr-3 mb-3">' +
+// //             '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-sm" name="shop-sizes"></span> <span class="d-inline-block text-black">Small</span>' +
+// //             '</label>' +
+// //             '<label for="option-md" class="d-flex mr-3 mb-3">' +
+// //             '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-md" name="shop-sizes"></span> <span class="d-inline-block text-black">Medium</span>' +
+// //             '</label>' +
+// //             '<label for="option-lg" class="d-flex mr-3 mb-3">' +
+// //             '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-lg" name="shop-sizes"></span> <span class="d-inline-block text-black">Large</span>' +
+// //             '</label>' +
+// //             '<label for="option-xl" class="d-flex mr-3 mb-3">' +
+// //             '<span class="d-inline-block mr-2" style="top:-2px; position: relative;"><input type="radio" id="option-xl" name="shop-sizes"></span> <span class="d-inline-block text-black"> Extra Large</span>' +
+// //             '</label>' +
+// //             '</div>' +
+// //             '<div class="mb-5">' +
+// //             '<div class="input-group mb-3" style="max-width: 120px;">' +
+// //             '<div class="input-group-prepend">' +
+// //             '<button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>' +
+// //             '</div>' +
+// //             '<input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">' +
+// //             '<div class="input-group-append">' +
+// //             '<button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>' +
+// //             '</div>' +
+// //             '</div>' +
+// //             '</div>' +
+// //             '<p><a href="" class="buy-now btn btn-sm btn-primary">Add To Cart</a></p>' +
+
+// //             '</div>' +
+// //             '</div>' +
+// //             '</div>' +
+// //             '</div>' +
+// //             $('#product_details').html(output);
+
+
+
+
+
+
+
+
+
+
+// //     })
+// //     .catch(function (error) {
+// //         console.log('Error:', error.message);
+// //     });
