068385eff5772d41e5326c913e3cf152560fee45
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-05 16:45:24 +0330

    chor : gallery instead of image

--- ShoesOnlineStore/shoes/views.py
+++ ShoesOnlineStore/shoes/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Subquery, OuterRef
 from django.views.generic.list import ListView
 from django.db.models import Q
-from .models import Product, Image
+from .models import Product, Gallery, Price
 from .serializer import ProductsSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # IsAuthenticatedOrReadOnly permission just can run safe method for unauthenticated user
@@ -41,6 +42,26 @@ class ProductSearchListView(APIView):
     serializer_class = ProductsSerializer
     queryset = Product.objects.filter(available_quantity__gte=1)
 
+
+class ProductList(APIView):
+    def get(self, request):
+        queryset = None
+        if not 'search' in request.GET:
+            queryset = Product.objects.filter(available_quantity__gte=1).select_related(
+                'brand', 'category').prefetch_related('color', 'size')
+        else:
+            queryset = Product.objects.filter(brand__title__icontains=request.GET.get('search'), available_quantity__gte=1).select_related(
+                'brand', 'category').prefetch_related('color', 'size')
+
+        # Subquery to get the last added price for each product
+        last_price_subquery = Price.objects.filter(
+            product=OuterRef('pk')).order_by('-created_at')
+        queryset = queryset.annotate(last_price=Subquery(
+            last_price_subquery.values('price')[:1]))
+
+        serializer = ProductsSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     # def get_queryset(self, **kwargs):
     #     print(222222222222)
     #     search = self.request.GET.get('search')
@@ -55,15 +76,15 @@ class ProductSearchListView(APIView):
     #     return Product.objects.all().select_related('brand', 'category').prefetch_related('color', 'size').filter(Q(title__contains=search) | Q(descriptions__contains=search))
 
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = None
-        if not 'search' in request.GET:
-            queryset = Product.objects.filter(available_quantity__gte=1).select_related(
-                'brand', 'category').prefetch_related('color', 'size')
-        else:
-            queryset = Product.objects.filter(brand__title__icontains=request.GET.get('search'), available_quantity__gte=1).select_related(
-                'brand', 'category').prefetch_related('color', 'size')
-        serializer = ProductsSerializer(
-            queryset, many=True)  # price in special model
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class ProductList(APIView):
+#     def get(self, request):
+#         queryset = None
+#         if not 'search' in request.GET:
+#             queryset = Product.objects.filter(available_quantity__gte=1).select_related(
+#                 'brand', 'category').prefetch_related('color', 'size')
+#         else:
+#             queryset = Product.objects.filter(brand__title__icontains=request.GET.get('search'), available_quantity__gte=1).select_related(
+#                 'brand', 'category').prefetch_related('color', 'size')
+#         serializer = ProductsSerializer(
+#             queryset, many=True)  # price in special model
+#         return Response(serializer.data, status=status.HTTP_200_OK)
