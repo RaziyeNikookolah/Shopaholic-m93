167dde47beb4cf17d9d3c2d264cb9b58731d2a28
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-05 16:50:03 +0330

    feat: impl SoftDeleted model

--- ShoesOnlineStore/core/models.py
+++ ShoesOnlineStore/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .manager import SoftDeleteManager
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
@@ -10,3 +12,25 @@ class BaseModel(models.Model):
         # ordering = ("-updated_at", "-created_at")
 # soft delete
 # class SoftDelete()
+
+
+class SoftDeleteModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(
+        null=True, default=None, blank=True, db_index=True)
+    objects = models.Manager()
+    undeleted_objects = SoftDeleteManager()
+    # deleted_objects =
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
+
+    class Meta:
+        abstract = True
