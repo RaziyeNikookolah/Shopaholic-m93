98932878cb22f9e3a69828e712ed9ca38eec136a
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-07 20:35:08 +0330

    feat:impl show_detail & shop_single

--- ShoesOnlineStore/home/views.py
+++ ShoesOnlineStore/home/views.py
@@ -1,6 +1,12 @@
+from django.core import serializers
 from django.shortcuts import render
 from django.views import View
-from shoes.models import Product
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from shoes.serializer import ProductsSerializer
+from django.db.models import OuterRef, Subquery
+from shoes.models import Product, Price
 
 
 class HomeView(View):
@@ -26,8 +32,8 @@ class ShopView(View):
 
 class CartView(View):
     def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        return render(request, "cart.html", {"product": product}
+        # product = Product.objects.get(pk=pk)
+        return render(request, "cart.html"  # , {"product": product}
                       )
 
 
@@ -45,16 +51,32 @@ class ContactView(View):
 
 class ShopSingleView(View):
     def get(self, request, pk):
-        product = Product.objects.filter(id=pk).select_related(
-            'brand', 'category').prefetch_related('color', 'size').first()
+        last_price_subquery = Price.objects.filter(
+            product=OuterRef('pk')).order_by('-create_timestamp')
+
+        product = Product.objects.filter(id=pk).select_related('brand', 'category').prefetch_related('color', 'size'
+                                                                                                     ).annotate(
+            last_price=Subquery(last_price_subquery.values('price')[:1])
+        ).first()
         sizes = product.size.all()
         colors = product.color.all()
-
-        return render(request, "shop_single.html", context={"product": product, 'sizes': sizes, "colors": colors}
-                      )
+        return render(request, "shop_single.html", {"product": product, "sizes": sizes, "colors": colors})
 
 
 class ThankyouView(View):
     def get(self, request):
         return render(request, "thankyou.html"
                       )
+
+
+class ShoeDetail(APIView):
+    def get(self, request, pk):
+        last_price_subquery = Price.objects.filter(
+            product=OuterRef('pk')).order_by('-create_timestamp')
+
+        product = Product.objects.filter(id=pk).select_related('brand', 'category').prefetch_related('color', 'size'
+                                                                                                     ).annotate(
+            last_price=Subquery(last_price_subquery.values('price')[:1])
+        ).first()
+        serializer = ProductsSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
