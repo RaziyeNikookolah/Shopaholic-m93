1bfafb71406ad737c4f89f5bc7b086b119dd0fc8
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-08 13:05:13 +0330

    chore:add cart_list in url

--- ShoesOnlineStore/orders/urls.py
+++ ShoesOnlineStore/orders/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
     path("remove_from_cart/", views.remove_from_cart, name="remove_from_cart"),
+    path("cart_list/", views.cart_list, name="cart_list"),
 ]
