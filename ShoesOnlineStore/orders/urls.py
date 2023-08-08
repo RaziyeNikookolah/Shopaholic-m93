f15933d5971701573e8de4d5a0037e08990f7305
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-08 11:49:11 +0330

    chore: add remove_from_cart url

--- ShoesOnlineStore/orders/urls.py
+++ ShoesOnlineStore/orders/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
+    path("remove_from_cart/", views.remove_from_cart, name="remove_from_cart"),
 ]
