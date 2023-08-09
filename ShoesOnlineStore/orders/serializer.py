3db99aeb58c19022fe48515fd2458237ed8e5c30
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-09 08:34:15 +0330

    chore : add const var

--- ShoesOnlineStore/orders/serializer.py
+++ ShoesOnlineStore/orders/serializer.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from .models import OrderItems
 from shoes.serializer import ProductsSerializer
+from django.conf import settings
+
+MAX_DIGITS = settings.MAX_DIGITS
+DECIMAL_PLACES = settings.DECIMAL_PLACES
 
 
 class OrderItemsSerializer(serializers.ModelSerializer):
@@ -13,10 +17,25 @@ class OrderItemsSerializer(serializers.ModelSerializer):
 
 class AddCartItemsSerializer(serializers.Serializer):
     product_id = serializers.CharField(max_length=4)
+    image = serializers.CharField(max_length=240)
     quantity = serializers.CharField(max_length=4)
     color = serializers.CharField(max_length=40)
     size = serializers.IntegerField()
+    price = serializers.DecimalField(
+        max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
 
 
 class RemoveCartItemsSerializer(serializers.Serializer):
     product_id = serializers.CharField(max_length=4)
+
+
+class CartItemSerializer(serializers.Serializer):
+
+    image = serializers.CharField(max_length=240)
+    quantity = serializers.CharField(max_length=4)
+    color = serializers.CharField(max_length=40)
+    size = serializers.IntegerField()
+    price = serializers.DecimalField(
+        max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    total_price = serializers.DecimalField(
+        max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
