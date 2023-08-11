8c831dfc0207fd8f097e15e541560854ae7c4660
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-11 18:59:37 +0330

    feat:furtunatly mk session independ of req to pass

--- ShoesOnlineStore/orders/views.py
+++ ShoesOnlineStore/orders/views.py
@@ -1,14 +1,37 @@
+from django.contrib.sessions.backends.db import SessionStore
+from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
-from orders.serializer import OrderItemsSerializer, AddCartItemsSerializer, RemoveCartItemsSerializer, CartItemSerializer
-from django.db.models import Subquery, OuterRef
-from shoes.models import Product, Price
-from .cart import Cart, CART_SESSION_ID
+from orders.serializer import AddCartItemsSerializer, RemoveCartItemsSerializer, CartItemSerializer
+# from django.db.models import Subquery, OuterRef
+# from shoes.models import Product, Price
+# from .cart import Cart, CART_SESSION_ID
+# Create a new session
+session = SessionStore()
 
 
-cart = Cart()
+def add_product_to_session(session_key, id, price, quantity):
+    global session
+    if session_key not in session:
+        session = SessionStore(session_key=session_key)
+
+    if session_key not in session:
+        session[session_key] = []
+
+    product = {
+        'id': id,
+        'price': str(price),
+        'quantity': quantity
+    }
+    for item in session[session_key]:
+        if item['id'] == id:
+            session[session_key].remove(item)
+    session[session_key].append(product)
+    session.save()
+
+    return session.session_key
 
 
 @csrf_exempt
@@ -16,29 +39,16 @@ cart = Cart()
 def add_to_cart(request):
     serializer = AddCartItemsSerializer(data=request.data)
     if serializer.is_valid():
-        # global cart
-
-        session = request.session
-        cart = session.get(CART_SESSION_ID)
-        if not cart:
-            cart = session[CART_SESSION_ID] = {}
 
         product_id = serializer.validated_data.get('product_id', '')
         quantity = serializer.validated_data.get('quantity', '')
-        color = serializer.validated_data.get('color', '')
-        image = serializer.validated_data.get('image', '')
-        size = serializer.validated_data.get('size', '')
         price = serializer.validated_data.get('price', '')
 
-        last_price_subquery = Price.objects.filter(
-            product=OuterRef('pk')).order_by('-create_timestamp')
+        add_product_to_session('products', product_id, price, quantity)
 
-        product = Product.objects.filter(id=product_id).select_related('brand', 'category').prefetch_related('color', 'size'
-                                                                                                             ).annotate(
-            last_price=Subquery(last_price_subquery.values('price')[:1])
-        ).filter(color__name=color, size__number=size).first()
+        # for item in session['products']:
+        #     print(item)
 
-        cart.add(product, quantity, size, image, color, str(price))
         return Response({'message': 'item_added'}, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -50,7 +60,13 @@ def remove_from_cart(request):
     serializer = RemoveCartItemsSerializer(data=request.data)
     product_id = serializer.validated_data.get('product_id', '')
     if serializer.is_valid():
-        cart.remove(product_id)
+        # cart.remove(product_id)
+        global cart
+        if product_id in cart:
+            del cart[product_id]
+            global session
+            session.modified = True
+
         return Response({'message': 'cart item removed'}, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -59,8 +75,9 @@ def remove_from_cart(request):
 @api_view(['GET'])
 def cart_list(request):
     global cart
-    cart.print()
+    for c in cart.items():
+        print(c)
+
     serializer = CartItemSerializer(cart, many=True)
     serialized_data = serializer.data
-
     return Response(serialized_data)
