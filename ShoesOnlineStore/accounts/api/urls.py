f9cd32e3c63683974ae6247a27d1b75be4d3b48a
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-13 17:18:15 +0330

    chore:add optain_pair_tokens url

--- ShoesOnlineStore/accounts/api/urls.py
+++ ShoesOnlineStore/accounts/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('access_token/', views.AccessTokenView.as_view(), name="access_token"),
     path('refresh_token/', views.RefreshTokenView.as_view(), name="refresh_token"),
+    path('optain_pair_tokens/', views.TokenObtainPairView.as_view(),
+         name="optain_pair_tokens"),
 
 ]
