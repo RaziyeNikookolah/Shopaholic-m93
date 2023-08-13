b1e8c15f6397384d469ce5b401df67b5159389e4
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-13 11:13:55 +0330

    feat: add zarinpal urls

--- ShoesOnlineStore/orders/urls.py
+++ ShoesOnlineStore/orders/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
     path("remove_from_cart/", views.remove_from_cart, name="remove_from_cart"),
     path("cart_list/", views.cart_list, name="cart_list"),
+    path('request_payment/', views.request_payment, name='request_payment'),
+    path('verify_payment/', views.verify_payment, name='verify_payment'),
 ]
