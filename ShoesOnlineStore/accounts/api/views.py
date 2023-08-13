63e9bc0a53ed834ee20836be68d56a96065467e5
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-13 17:14:09 +0330

    feat : add 3 classview for jwt token

---
+++ ShoesOnlineStore/accounts/api/views.py
@@ -0,0 +1,35 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView, status
+from accounts.utils import generate_access_token, generate_refresh_token
+
+
+class TokenObtainPairView(APIView):
+
+    def get(self, request):
+        if user := request.user:
+            access_token = generate_access_token(user, 3)
+            refresh_token = generate_refresh_token(user, 30)
+            return Response({"access_token": access_token, "refresh_token": refresh_token}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+
+
+class AccessTokenView(APIView):
+
+    def get(self, request):
+        if user := request.user:
+            access_token = generate_access_token(user, 3)
+            return Response({"access_token": access_token}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+
+
+class RefreshTokenView(APIView):
+
+    def get(self, request):
+        if user := request.user:
+            refresh_token = generate_refresh_token(user, 3)
+            return Response({"refresh_token": refresh_token}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
