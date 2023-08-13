2b7892dd3005d4877989f0a1e3af136597f50d7e
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-13 11:14:21 +0330

    add zarinpal view

--- ShoesOnlineStore/orders/views.py
+++ ShoesOnlineStore/orders/views.py
@@ -5,10 +5,32 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from .utils import add_product_to_session, session_cart, remove_product_from_session
 from orders.serializer import RemoveCartItemsSerializer, CartItemSerializer, OrderItemsSerializer
+from django.conf import settings
+import requests
+import json
 # from django.db.models import Subquery, OuterRef
 # from shoes.models import Product, Price
 # from .cart import Cart, CART_SESSION_ID
 # Create a new session
+
+
+# ? sandbox merchant
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+autho = ""
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+
+amount = 1000  # Rial / Required
+description = "نهایی کردن خرید شما"  # Required
+phone = 'YOUR_PHONE_NUMBER'  # Optional
+# Important: need to edit for realy server.
+CallbackURL = 'http://127.0.0.1:8080/order/verify_payment/'
 
 
 @csrf_exempt
@@ -52,3 +74,57 @@ def cart_list(request):
 @api_view(['POST'])
 def order_create(request):
     ...
+
+
+def request_payment(request):
+    data = {
+        "MerchantID": settings.MERCHANT,
+        "Amount": amount,
+        "Description": description,
+        "Phone": phone,
+        "CallbackURL": CallbackURL,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json',
+               'content-length': str(len(data))}
+    try:
+        response = requests.post(
+            ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+        if response.status_code == 200:
+            response = response.json()
+            global autho
+            autho = str(response['Authority'])
+            if response['Status'] == 100:
+                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']), 'authority': response['Authority']}
+            else:
+                return {'status': False, 'code': str(response['Status'])}
+        return response
+
+    except requests.exceptions.Timeout:
+        return {'status': False, 'code': 'timeout'}
+    except requests.exceptions.ConnectionError:
+        return {'status': False, 'code': 'connection error'}
+
+
+def verify_payment(request):
+    global autho
+    data = {
+        "MerchantID": settings.MERCHANT,
+        "Amount": amount,
+        "Authority": autho,
+    }
+    data = json.dumps(data)
+    # set content length by data
+    headers = {'content-type': 'application/json',
+               'content-length': str(len(data))}
+    response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+
+    if response.status_code == 200:
+        response = response.json()
+        if response['Status'] == 100:
+            return {'status': True, 'RefID': response['RefID']}
+        else:
+            return {'status': False, 'code': str(response['Status'])}
+    return response
