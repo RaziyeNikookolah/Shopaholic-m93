aacb66d7c37b51dc34a0fdc9bc6713ebb26f7850
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-13 15:02:59 +0330

    feat:generate access & refresh token func in utils

---
+++ ShoesOnlineStore/accounts/utils.py
@@ -0,0 +1,30 @@
+import jwt
+import datetime
+from django.conf import settings
+
+
+def generate_access_token(user, expiration_time_minutes=7):
+    access_token_payload = {
+        "user_id": user.id,
+        "exp": datetime.datetime.utcnow()
+        + datetime.timedelta(days=0, minutes=expiration_time_minutes),
+        "iat": datetime.datetime.utcnow(),
+    }
+    access_token = jwt.encode(
+        access_token_payload, settings.SECRET_KEY, algorithm="HS256"
+    )
+
+    return access_token
+
+
+def generate_refresh_token(user, expiration_time_days=7):
+    refresh_token_payload = {
+        "user_id": user.id,
+        "exp": datetime.datetime.utcnow()
+        + datetime.timedelta(days=expiration_time_days),
+        "iat": datetime.datetime.utcnow(),
+    }
+    refresh_token = jwt.encode(
+        refresh_token_payload, settings.REFRESH_TOKEN_SECRET, algorithm="HS256"
+    )
+    return refresh_token
