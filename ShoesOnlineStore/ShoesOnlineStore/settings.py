fb83ccc594a6f046243ad191aa21d5426c9feb27
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-14 11:12:20 +0330

    feat:mk jwt auth defualt auth for all req

--- ShoesOnlineStore/ShoesOnlineStore/settings.py
+++ ShoesOnlineStore/ShoesOnlineStore/settings.py
@@ -157,25 +157,25 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    # "DEFAULT_AUTHENTICATION_CLASSES": ("accounts.authentication.JWTAuthentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("accounts.authentication.JWTAuthentication",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 3,
 }
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
 
-    ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    # it makes defualt for all methods
-    # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    # ]
-}
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#     it makes defualt for all methods
+#     'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+#     ]
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
