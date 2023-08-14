dc7e51420f1ebf11e977cca0e6a1e28608bde87d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-14 11:17:59 +0330

    chore: mod all method view class base view

--- ShoesOnlineStore/orders/views.py
+++ ShoesOnlineStore/orders/views.py
@@ -1,17 +1,11 @@
-from django.http import HttpResponse
+
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 from .utils import add_product_to_session, session_cart, remove_product_from_session
 from orders.serializer import RemoveCartItemsSerializer, CartItemSerializer, OrderItemsSerializer
-from django.conf import settings
-import requests
-import json
-# from django.db.models import Subquery, OuterRef
-# from shoes.models import Product, Price
-# from .cart import Cart, CART_SESSION_ID
-# Create a new session
 
 
 # # ? sandbox merchant
@@ -33,47 +27,53 @@ import json
 # CallbackURL = 'http://127.0.0.1:8080/order/verify_payment/'
 
 
-@csrf_exempt
-@api_view(['POST'])
-def add_to_cart(request):
+class AddToCartView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
 
-    serializer = CartItemSerializer(data=request.data)
+    @csrf_exempt
+    def post(self, request, format=None):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            product_id = serializer.validated_data.get('product_id', '')
+            quantity = serializer.validated_data.get('quantity', '')
+            price = serializer.validated_data.get('price', '')
+            total_price = serializer.validated_data.get('total_price', '')
+            add_product_to_session(product_id, price, quantity, total_price)
 
-    if serializer.is_valid():
-        product_id = serializer.validated_data.get('product_id', '')
-        quantity = serializer.validated_data.get('quantity', '')
-        price = serializer.validated_data.get('price', '')
-        total_price = serializer.validated_data.get('total_price', '')
-        add_product_to_session(product_id,
-                               price, quantity, total_price)
-
-        return Response({'message': 'item_added'}, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@csrf_exempt
-@api_view(['POST'])
-def remove_from_cart(request):
-    print(111111)
-    serializer = RemoveCartItemsSerializer(data=request.data)
-    if serializer.is_valid():
-        product_id = serializer.validated_data.get('product_id', '')
-        print(22222)
-        return remove_product_from_session(product_id)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'item_added'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def cart_list(request):
-    session_whit_serialized_product = session_cart()
-    return Response(session_whit_serialized_product)
+class RemoveCartItemView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
+    @csrf_exempt
+    def post(self, request):
+        serializer = RemoveCartItemsSerializer(data=request.data)
+        if serializer.is_valid():
+            product_id = serializer.validated_data.get('product_id', '')
+            return remove_product_from_session(product_id)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-def order_create(request):
-    ...
+class CartListView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        session_with_serialized_product = session_cart()
+        return Response(session_with_serialized_product)
+
+
+class OrderCreateView(APIView):
+    def post(self, request, format=None):
+        # Your logic for order creation here
+        ...
+        return Response("Order created successfully.")
 
 
 # def request_payment(request):
