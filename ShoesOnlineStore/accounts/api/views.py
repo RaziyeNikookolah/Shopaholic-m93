4c2164d76bfdee46486a40580d220ee5e63083b2
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-15 22:14:28 +0330

    chore: coment token views exept tokenOptainPair

--- ShoesOnlineStore/accounts/api/views.py
+++ ShoesOnlineStore/accounts/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView, status
+from rest_framework.permissions import AllowAny
 from accounts.utils import generate_access_token, generate_refresh_token
 
 
@@ -15,21 +16,25 @@ class TokenObtainPairView(APIView):
             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
 
 
-class AccessTokenView(APIView):
+# class AccessTokenView(APIView):
 
-    def get(self, request):
-        if user := request.user:
-            access_token = generate_access_token(user, 3)
-            return Response({"access_token": access_token}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+#     def get(self, request):
+#         if user := request.user:
+#             access_token = generate_access_token(user, 3)
+#             return Response({"access_token": access_token}, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
 
 
-class RefreshTokenView(APIView):
+# class RefreshTokenView(APIView):
 
-    def get(self, request):
-        if user := request.user:
-            refresh_token = generate_refresh_token(user, 3)
-            return Response({"refresh_token": refresh_token}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+#     def get(self, request):
+#         if user := request.user:
+#             refresh_token = generate_refresh_token(user, 3)
+#             return Response({"refresh_token": refresh_token}, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+# class LoginOrRegisterView(APIView):
+#     permission_classes=[AllowAny]
+#     authentication_classes=[]
+#     def post(self,request):
