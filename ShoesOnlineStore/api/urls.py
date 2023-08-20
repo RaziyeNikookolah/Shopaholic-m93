a12e95f6195fac57f77ec1b26e8cce21376e4396
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-21 00:10:06 +0330

    add shoes api router to url

--- ShoesOnlineStore/api/urls.py
+++ ShoesOnlineStore/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
+from shoes.api.views import router as shoes_router
 
 
 urlpatterns = [
-    path('accounts/', include("accounts.api.urls")),]
+    path('accounts/', include("accounts.api.urls")),
+    path('shoes/', include(shoes_router.urls)),
+
+]
