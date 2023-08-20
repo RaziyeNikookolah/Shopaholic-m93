4b5509c477b513b31a57cdf8f6e09a71c8a6d192
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-20 23:59:47 +0330

    del:get rid of comented code

--- ShoesOnlineStore/shoes/urls.py
+++ ShoesOnlineStore/shoes/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('product_list/', views.ProductList.as_view(), name='product_list'),
-    path('search_list/',
-         views.ProductSearchListView.as_view(), name='search_list'),
-    path('product_update/<int:pk>/',
-         views.ProductUpdate.as_view(), name='product_update'),
+    #     path('search_list/',
+    #          views.ProductSearchListView.as_view(), name='search_list'),
+    #     path('product_update/<int:pk>/',
+    #          views.ProductUpdate.as_view(), name='product_update'),
 ]
