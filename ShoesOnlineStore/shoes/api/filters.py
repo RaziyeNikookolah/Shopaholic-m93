356b451720b486d781de7c58a404497f77c12906
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-20 23:42:52 +0330

    feat:impl filters.py for product

---
+++ ShoesOnlineStore/shoes/api/filters.py
@@ -0,0 +1,18 @@
+import django_filters
+from ..models import Product
+
+
+class ProductFilter(django_filters.FilterSet):
+    price = django_filters.RangeFilter()
+
+    class Meta:
+        model = Product
+        fields = {
+            'title': ['icontains'],
+            'brand__title': ['icontains'],
+            'brand__manufacturing_country': ['icontains'],
+            'descriptions': ['icontains'],
+            'size': ['icontains'],
+            'color': ['exact'],
+            # 'price':['lt','gt'],
+        }
