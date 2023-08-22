5ecfc974305bdb64da2a65278676c1c69421b438
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-22 09:38:13 +0330

    chore:add logout url

--- ShoesOnlineStore/accounts/urls.py
+++ ShoesOnlineStore/accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("request_logout/", views.RequestLogoutView.as_view(), name="request_logout"),
     path("send_email_to_all/", views.send_mail_to_all, name="send_email_to_all"),
     path('', views.test, name="test"),
     path('sendmail/', views.send_mail_to_all, name="sendmail"),
