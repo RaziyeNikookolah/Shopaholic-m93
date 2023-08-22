3c251d19462f7f7c53af1bd0db0e8a356843aa1d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-22 12:21:43 +0330

    chore: adda function & request for colors

--- ShoesOnlineStore/statics/js/shop_script.js
+++ ShoesOnlineStore/statics/js/shop_script.js
@@ -23,12 +23,12 @@ $(document).ready(function () {
                         '</figure>' +
                         '<div class="block-4-text p-4">' +
                         '<h3 "><a  href="/shop_single/' + element.id + '/">' + element.brand + '</a></h3>' +
+                        '<h5>' + element.category + '</h5>' +
                         '<p class="mb-0">' + element.descriptions + '</p>' +
                         '<p class="text-primary font-weight-bold">$' + element.last_price + '</p>' +
                         '</div>' +
                         '</div>' +
                         '</div>';
-
 
                 });
                 output += '</div>';
@@ -46,6 +46,7 @@ $(document).ready(function () {
 
     // Initial load of the first page
     loadPageData('http://localhost:8000/shoes/product_list/');
+
 
     // Add an event listener for the "Next" button
     $('#next_page').on('click', function (e) {
@@ -107,48 +108,84 @@ $(document).ready(function () {
 
 
     $('#search_form').on('keypress', function (e) {
-        e.preventDefault();
         if (e.keyCode == 13) {
             e.preventDefault();
             accessToken = window.localStorage.getItem('accessToken');
+            loadPageData('http://127.0.0.1:8000/shoes/product_list/?search=' + $('#search_form').val());
 
-            $.ajax({
-                url: 'http://127.0.0.1:8000/shoes/product_list/?search=' + $('#search_form').val(),
-                type: 'GET',
-                dataType: "json",
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-                },
-                success: function (data, status, xhr) {
-
-                    output = '<div class="row mb-5" >'
-                    data.forEach((element, index) => {
-                        // console.log(`Element ${index}:`, element);
-                        output += '<div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">' +
-                            '<div class="block-4 text-center border">' +
-                            '<figure class="block-4-image">' +
-                            ' <a href=""><img src="' + element.image + '"  alt="Image placeholder" class="img-fluid"></a>' +
-                            '</figure>' +
-                            '<div class="block-4-text p-4">' +
-                            '<h3 "><a  href="/shop_single/' + element.id + '/">' + element.brand + '</a></h3>' +
-                            '<p class="mb-0">' + element.descriptions + '</p>' +
-                            '<p class="text-primary font-weight-bold">$' + element.price + '</p>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>';
-                    });
-                    output += '</div>';
-                    $('#all_products').html(output);
-                    // window.location = 'http://127.0.0.1:8000';
-
-                },
-                error: function (jqXhr, textStatus, errorMessage) { // error callback 
-                    console.log('Error in loading data', errorMessage);
-                },
-            });
         }
 
     });
+
+    function bootstrapColor(color) {
+
+        switch (color.toLowerCase()) {
+            case 'blue':
+            case 'darkBlue':
+                return 'bg-primary';
+            case 'gray':
+                return 'bg-secondary';
+            case 'green':
+                return 'bg-success';
+            case 'red':
+                return 'bg-danger';
+            case 'yellow':
+                return 'bg-warning';
+            case 'lightBlue':
+                return 'bg-info';
+            case 'light':
+                return 'bg-light';
+            case 'black':
+                return 'bg-dark';
+            case 'white':
+                return 'bg-white';
+            case 'purple':
+                return 'bg-purple'
+            default:
+                return '';
+        }
+
+    }
+
+
+    var div_color = $('#colors');
+
+    function getColors() {
+        var div_color = $('#colors');
+        accessToken = window.localStorage.getItem('accessToken');
+        $.ajax({
+            url: 'http://127.0.0.1:8000/api/v1/shoes/colors/',
+            type: 'GET',
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+            },
+            success: function (response) {
+                var output = '<h3 class="mb-3 h6 text-uppercase text-black d-block">Color</h3>';
+                for (var colorKey in response) {
+                    var color = response[colorKey];
+
+                    output += '<a href="#" class="d-flex color-item align-items-center" id="color_' + color.id + '">' +
+                        '<span class="' + bootstrapColor(color.name) + ' color d-inline-block rounded-circle mr-2"></span> <span class="text-black">' + color.name + '</span>' +
+                        '</a>';
+                }
+                div_color.html(output);
+
+                // Attach click event using jQuery
+                $('[id^="color_"]').on('click', function () {
+                    var colorText = $(this).find('span:last-child').text();
+                    console.log('Clicked Color:', colorText);
+
+                    loadPageData('http://127.0.0.1:8000/shoes/product_list/?title=&category__title=&brand__title=&size__number=&color__name=' + colorText);
+                });
+            },
+            error: function (error) {
+                console.error('Error:', error);
+            }
+        });
+    }
+    getColors();
+
     getCategories();
     var ul = $('#ulCategories');
     function getCategories() {
@@ -175,6 +212,8 @@ $(document).ready(function () {
                 $('[id^="category_"]').on('click', function () {
                     var categoryText = $(this).find('span:first-child').text();
                     console.log('Clicked Category:', categoryText);
+
+                    loadPageData('http://localhost:8000/shoes/product_list/?category__title=' + categoryText);
                 });
             },
             error: function (error) {
