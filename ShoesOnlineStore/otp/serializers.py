dadf76728a4ac265b5f7168b4c2416ffeb55878d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-24 23:15:38 +0330

    feat:impl validate_phone_number

--- ShoesOnlineStore/otp/serializers.py
+++ ShoesOnlineStore/otp/serializers.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+import re
 from rest_framework import serializers
 from .models import OtpRequest
 
@@ -8,12 +8,14 @@ class RequestOtpSerializer(serializers.ModelSerializer):
         model = OtpRequest
         fields = ['phone_number']
 
+    def validate_phone_number(self, value):
+        regex = re.compile(r'^(?:\+98|0|98)?9\d{9}$')
+        if not regex.match(value):
+            raise serializers.ValidationError("Invalid phone number")
+        return value
+
 
 class VerifyOtpSerializer(serializers.Serializer):
     # request_id = serializers.CharField(max_length=64)
     phone_number = serializers.CharField(max_length=12)
     code = serializers.CharField(max_length=4)
-
-
-# class TokenSerializer(serializers.Serializer):
-#     token = serializers.CharField(max_length=250)
