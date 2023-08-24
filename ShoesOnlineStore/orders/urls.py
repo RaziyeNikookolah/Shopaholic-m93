dae6375ac77824c4650b67d89e64271236cb7330
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-24 23:12:57 +0330

    chore clean code

--- ShoesOnlineStore/orders/urls.py
+++ ShoesOnlineStore/orders/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from . import views, zarrinpal_views
+from . import views, zarinpal_views
 
 
 urlpatterns = [
@@ -14,16 +14,10 @@ urlpatterns = [
          name="update_cart"),
     path("checkout/", views.CheckoutView.as_view(), name="checkout"),
     path("create_order/", views.CreateOrder.as_view(), name="create_order"),
-    path("export/", views.export, name='export'),
+    #     path("export/", views.export, name='export'),
 
-    #     path('request_payment/', views.ZarrinPalRequestPayment.as_view(),
-    #          name='request_payment'),
-    #     path('verify_payment/', views.ZarrinPalVerifyPayment.as_view(),
-    #          name='verify_payment'),
-
-    #     path('request-payment/', zarinpal_views.request_payment, name='request_payment'),
-    #     path('verify-payment/', zarinpal_views.verify_payment, name='verify_payment')
-    path('request/', zarrinpal_views.send_request, name='request'),
-    path('verify/', zarrinpal_views.verify, name='verify'),
+    path('request/<int:order_id>/',
+         zarinpal_views.PaymentRequest.as_view(), name='request'),
+    path('verify/', zarinpal_views.PaymentVerify.as_view(), name='verify'),
 
 ]
--- ShoesOnlineStore/orders/views.py
+++ ShoesOnlineStore/orders/views.py
@@ -16,74 +16,6 @@ from .utils import add_product_to_session, session_cart, remove_product_from_ses
 from orders.serializer import RemoveCartItemsSerializer, CartItemSerializer, OrderItemsSerializer
 
 
-# ZP_API_REQUEST = "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
-# ZP_API_VERIFY = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
-# ZP_API_STARTPAY = "https://sandbox.zarinpal.com/pg/StartPay/"
-
-# amount = 1000  # Rial / Required
-# description = "نهایی کردن خرید شما"  # Required
-# # phone = 'YOUR_PHONE_NUMBER'  # Optional
-# # Important: need to edit for realy server.
-# CallbackURL = 'http://127.0.0.1:8080/order/verify_payment/'
-
-
-# class ZarrinPalRequestPayment(View):
-#     # authentication_classes = []
-#     # permission_classes = (AllowAny,)
-
-#     def get(self, request):
-#         data = {
-#             "MerchantID": settings.MERCHANT,
-#             "Amount": 1000,
-#             "Description": "Finalize your order",
-#             # "Phone": phone,
-#             "CallbackURL": 'http://127.0.0.1:8080/order/verify_payment/'
-#             # 'http://127.0.0.1:8000/thank_you/',
-#         }
-#         data = json.dumps(data)
-#         # set content length by data
-#         headers = {"accept": "application/json", 'content-type': 'application/json',
-#                    #    'content-length': str(len(data))
-#                    }
-
-#         req = requests.post(
-#             ZP_API_REQUEST, data=data, headers=headers, timeout=10)
-
-#         # authority = req.json()['data']['authority']
-#         # if len(req.json()['errors']) == 0:
-#         return redirect(ZP_API_STARTPAY.format(authority=autho))
-#         # else:
-#         # e_code = req.json()['errors']['code']
-#         # e_message = req.json()['errors']['message']
-#         # f"Error code: {e_code}, Error Message: {e_message}")
-#         # return HttpResponse("error")
-
-
-# class ZarrinPalVerifyPayment(View):
-#     # authentication_classes = []
-#     # permission_classes = (AllowAny,)
-
-#     def get(self, request):
-#         global autho
-#         data = {
-#             "MerchantID": settings.MERCHANT,
-#             "Amount": amount,
-#             "Authority": autho,
-#         }
-#         data = json.dumps(data)
-#         # set content length by data
-#         headers = {'content-type': 'application/json',
-#                    'content-length': str(len(data))}
-#         response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
-
-#         if response.status_code == 200:
-#             response = response.json()
-#             if response['Status'] == 100:
-#                 return {'status': True, 'RefID': response['RefID']}
-#             else:
-#                 return {'status': False, 'code': str(response['Status'])}
-#         return response
-
 logger = logging.getLogger('ShoesOnlineStore.orders')
 
 
@@ -192,29 +124,29 @@ class CreateOrder(APIView):
             create_orderItems_from_session(order)
             # clear_session() not here it will be done when order paid
             total_price = order.get_total_price()
-            return Response({"total_price": total_price, 'message': "Order added successfully"}, status=status.HTTP_201_CREATED)
+            return Response({"order_id": order.id, "total_price": total_price, 'message': "Order added successfully"}, status=status.HTTP_201_CREATED)
 
         return Response({'message': "Error"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-def export(request):  # no one can not call this view use user_passes_test
-    response = HttpResponse(content_type='text/csv')
-    writer = csv.writer(response)
-    writer.writerow(['Products in order:'])
-    today = timezone.now().date()
+# def export(request):  # no one can not call this view use user_passes_test
+#     response = HttpResponse(content_type='text/csv')
+#     writer = csv.writer(response)
+#     writer.writerow(['Products in order:'])
+#     today = timezone.now().date()
 
-    orders_created_today = Order.objects.filter(create_timestamp__date=today).select_related('items').values(
-        'id', 'create_timestamp', 'is_paid', 'receiver_name', 'receiver_lastname', 'city')
+#     orders_created_today = Order.objects.filter(create_timestamp__date=today).select_related('items').values(
+#         'id', 'create_timestamp', 'is_paid', 'receiver_name', 'receiver_lastname', 'city')
 
-    for order in orders_created_today:
-        writer.writerow([order['create_timestamp'], order['is_paid'],
-                        order['receiver_name'], order['receiver_lastname'], order['city']])
+#     for order in orders_created_today:
+#         writer.writerow([order['create_timestamp'], order['is_paid'],
+#                         order['receiver_name'], order['receiver_lastname'], order['city']])
 
-        items = OrderItem.objects.filter(order_id=order['id'])
-        for item in items:
+#         items = OrderItem.objects.filter(order_id=order['id'])
+#         for item in items:
 
-            writer.writerow([item.product, item.quantity, item.final_price])
+#             writer.writerow([item.product, item.quantity, item.final_price])
 
-    response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+#     response['Content-Disposition'] = 'attachment; filename="orders.csv"'
 
-    return response
+#     return response
