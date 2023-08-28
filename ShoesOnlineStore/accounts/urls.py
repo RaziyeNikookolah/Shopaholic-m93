d39ddabdf17fa9b8a62596876af3d9751bc1fac1
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:41:52 +0330

    chore : clean accounts urls

--- ShoesOnlineStore/accounts/urls.py
+++ ShoesOnlineStore/accounts/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path("profile/", views.ProfileView.as_view(), name="profile"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("request_logout/", views.RequestLogoutView.as_view(), name="request_logout"),
-    path("send_email_to_all/", views.send_mail_to_all, name="send_email_to_all"),
-    path('', views.test, name="test"),
-    path('sendmail/', views.send_mail_to_all, name="sendmail"),
-    path('schedulemail/', views.schedule_mail, name="schedulemail"),
 ]
