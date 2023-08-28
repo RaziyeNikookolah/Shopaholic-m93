7f107def215024d63479f74c071cafd5b4c656a6
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:27:11 +0330

    chore : clean code in order urls

--- ShoesOnlineStore/orders/urls.py
+++ ShoesOnlineStore/orders/urls.py
@@ -5,16 +5,14 @@ from . import views, zarinpal_views
 
 urlpatterns = [
 
-    path("create/", views.OrderCreateView.as_view(), name="order_create"),
     path("cart_list/", views.CartListView.as_view(), name="cart_list"),
     path("add_to_cart/", views.AddToCartView.as_view(), name="add_to_cart"),
-    path("remove_from_cart/", views.RemoveCartItemView.as_view(),
-         name="remove_from_cart"),
+    #     path("remove_from_cart/", views.RemoveCartItemView.as_view(),
+    #     name="remove_from_cart"),
     path("update_cart/", views.UpdateCartItemView.as_view(),
          name="update_cart"),
     path("checkout/", views.CheckoutView.as_view(), name="checkout"),
     path("create_order/", views.CreateOrder.as_view(), name="create_order"),
-    #     path("export/", views.export, name='export'),
 
     path('request/<int:order_id>/',
          zarinpal_views.PaymentRequest.as_view(), name='request'),
