6b4b4063b684fa9805684616c9b7afd5311e7a19
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:16:35 +0330

    chore : clean shoes urls

--- ShoesOnlineStore/shoes/urls.py
+++ ShoesOnlineStore/shoes/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('product_list/', views.ProductList.as_view(), name='product_list'),
-    #     path('search_list/',
-    #          views.ProductSearchListView.as_view(), name='search_list'),
-    #     path('product_update/<int:pk>/',
-    #          views.ProductUpdate.as_view(), name='product_update'),
+    path('product_details/<int:pk>/',
+         views.ProductDetails.as_view(), name='product_details'),
+
 ]
