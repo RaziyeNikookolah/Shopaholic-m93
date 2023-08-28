39d809b053db0a3c6006f2d6acfb014b3ca9d45a
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:14:33 +0330

    chore : impl shoes model test

--- ShoesOnlineStore/shoes/test/test_models.py
+++ ShoesOnlineStore/shoes/test/test_models.py
@@ -1,37 +1,91 @@
 from django.test import TestCase
-from ..models import Product, Brand, Quantity, Size, Color, Gallery, Category, Price
+from model_bakery import baker
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from ..models import Product, Brand, Quantity, Size, Color, Category, Price, SHOES_SIZE_NAME
 
 
-class ProductModelTest(TestCase):
-    def test_str_representation(self):
-        brand = Brand.objects.create(title="Nike", manufacturing_country="USA")
-        category = Category.objects.create(title="Footwear")
+class ProductModelTestCase(TestCase):
+
+    def test_product_creation(self):
+        brand = baker.make(Brand)
+        category = baker.make(Category)
+        color = baker.make(Color)
+        size = baker.make(Size)
+
         product = Product.objects.create(
-            title="Running Shoes", brand=brand, code="ABC123", category=category
+            title="Test Product",
+            brand=brand,
+            code="TP123",
+            descriptions="Test description",
+            available_quantity=10,
+            category=category,
+            image="test_image.jpg",
+            is_active=True,
+            slug="test-product",
         )
+
+        product.color.add(color)
+        product.size.add(size)
+
         self.assertEqual(str(product), f"{product.code},{product.brand}")
 
+    def test_product_unique_code(self):
+        brand = baker.make(Brand)
+        category = baker.make(Category)
 
-class BrandModelTest(TestCase):
-    def test_str_representation(self):
-        brand = Brand.objects.create(title="Nike", manufacturing_country="USA")
-        self.assertEqual(str(brand), brand.title)
-
-
-class QuantityModelTest(TestCase):
-    def test_str_representation(self):
-        brand = Brand.objects.create(title="Nike", manufacturing_country="USA")
-        category = Category.objects.create(title="Footwear")
-        product = Product.objects.create(
-            title="Running Shoes", brand=brand, code="ABC123", category=category
+        Product.objects.create(
+            title="Product 1",
+            brand=brand,
+            code="P123",
+            category=category,
         )
+
+        with self.assertRaises(IntegrityError):
+            Product.objects.create(
+                title="Product 2",
+                brand=brand,
+                code="P123",
+                category=category,
+            )
+
+
+class QuantityModelTestCase(TestCase):
+
+    def test_quantity_creation(self):
+        product = baker.make(Product)
         quantity = Quantity.objects.create(product=product, quantity=100)
         self.assertEqual(str(quantity), str(quantity.quantity))
 
 
-class SizeModelTest(TestCase):
-    def test_str_representation(self):
-        size = Size.objects.create(name=Size.SHOES_SIZE_NAME.ADULT, number=42)
+class SizeModelTestCase(TestCase):
+
+    def test_size_creation(self):
+        size = Size.objects.create(
+            name=SHOES_SIZE_NAME.ADULT, number=40)
         self.assertEqual(str(size), str(size.number))
 
-# Similarly, write test cases for other models
+
+class ColorModelTestCase(TestCase):
+
+    def test_color_creation(self):
+        color = Color.objects.create(name="Red")
+        self.assertEqual(str(color), color.name)
+
+
+class CategoryModelTestCase(TestCase):
+
+    def test_category_creation(self):
+        parent_category = baker.make(Category)
+        category = Category.objects.create(
+            parent_category=parent_category, title="Subcategory"
+        )
+        self.assertEqual(str(category), category.title)
+
+
+class PriceModelTestCase(TestCase):
+
+    def test_price_creation(self):
+        product = baker.make(Product)
+        price = Price.objects.create(product=product, price=99.99)
+        self.assertEqual(str(price), f'{price.create_timestamp}:{price.price}')
