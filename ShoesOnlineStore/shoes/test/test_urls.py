4c2fe0c960cbb0c6ce50c8fac8392e3c5bdcb975
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:11:22 +0330

    chor :test shoes  urls

--- ShoesOnlineStore/shoes/test/test_urls.py
+++ ShoesOnlineStore/shoes/test/test_urls.py
@@ -1,72 +1,18 @@
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from ..models import Product, Price, Brand, Category, Color, Size
+import unittest
+from django.urls import reverse, resolve
+from .. import views
 
 
-class ProductListViewTest(APITestCase):
-    def setUp(self):
-        # Create test data
-        self.brand = Brand.objects.create(title="Nike")
-        self.category = Category.objects.create(title="Footwear")
-        self.color = Color.objects.create(name="Red")
-        self.size = Size.objects.create(
-            name=Size.SHOES_SIZE_NAME.ADULT, number=10)
-
-        self.product1 = Product.objects.create(
-            title="Running Shoes", brand=self.brand,
-            code="ABC123", category=self.category,
-            available_quantity=10
-        )
-        self.product1.color.add(self.color)
-        self.product1.size.add(self.size)
-        self.price1 = Price.objects.create(product=self.product1, price=100)
-
-        self.product2 = Product.objects.create(
-            title="Sneakers", brand=self.brand,
-            code="XYZ456", category=self.category,
-            available_quantity=5
-        )
-        self.product2.color.add(self.color)
-        self.product2.size.add(self.size)
-        self.price2 = Price.objects.create(product=self.product2, price=80)
-
-    def test_product_list(self):
+class TestUrls(unittest.TestCase):
+    def test_product_list_url(self):
         url = reverse('product_list')
-        response = self.client.get(url)
+        self.assertEqual(resolve(url).func.view_class, views.ProductList)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+    def test_product_details_url(self):
+        # Assuming pk = 1 for testing purposes
+        url = reverse('product_details', args=[1])
+        self.assertEqual(resolve(url).func.view_class, views.ProductDetails)
 
-    def test_product_list_with_search(self):
-        url = reverse('product_list')
-        data = {'search': 'Running'}
-        response = self.client.get(url, data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], "Running Shoes")
-
-    def test_product_list_with_filters(self):
-        url = reverse('product_list')
-        data = {'brand__title': 'Nike', 'color__name': 'Red'}
-        response = self.client.get(url, data)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], "Running Shoes")
-
-    def test_product_list_caching(self):
-        url = reverse('product_list')
-
-        response1 = self.client.get(url)
-        self.assertEqual(response1.status_code, status.HTTP_200_OK)
-
-        # Modify data
-        self.product1.available_quantity = 5
-        self.product1.save()
-
-        response2 = self.client.get(url)
-        self.assertEqual(response2.status_code, status.HTTP_200_OK)
-
-        self.assertEqual(response1.content, response2.content)
+if __name__ == '__main__':
+    unittest.main()
