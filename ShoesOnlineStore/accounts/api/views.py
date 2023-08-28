734b0995584bb2d2620c33733cc16466a64be360
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:37:20 +0330

    chore verify viewset in accounts api

--- ShoesOnlineStore/accounts/api/views.py
+++ ShoesOnlineStore/accounts/api/views.py
@@ -1,7 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework.viewsets import ViewSet
+from ..models import Account
+from .serializers import AccountSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView, status
-from rest_framework.permissions import AllowAny
 from accounts.utils import generate_access_token, generate_refresh_token
 
 
@@ -16,25 +18,15 @@ class TokenObtainPairView(APIView):
             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
 
 
-# class AccessTokenView(APIView):
+class AccountViewSet(ViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    http_method_names = ['get']
 
-#     def get(self, request):
-#         if user := request.user:
-#             access_token = generate_access_token(user, 3)
-#             return Response({"access_token": access_token}, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
+    def list(self, request):
+        serializer = AccountSerializer(self.queryset, many=True)
+        return Response(serializer.data)
 
 
-# class RefreshTokenView(APIView):
-
-#     def get(self, request):
-#         if user := request.user:
-#             refresh_token = generate_refresh_token(user, 3)
-#             return Response({"refresh_token": refresh_token}, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response({"message": "No Token Generated "}, status=status.HTTP_204_NO_CONTENT)
-# class LoginOrRegisterView(APIView):
-#     permission_classes=[AllowAny]
-#     authentication_classes=[]
-#     def post(self,request):
+router = DefaultRouter()
+router.register(r'accounts', AccountViewSet, basename='account')
