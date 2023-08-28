e4cc64c38716a5b7233e64e4adc7e4fa065b4481
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-28 21:18:21 +0330

    chore:otp urls test

--- ShoesOnlineStore/otp/test/test_urls.py
+++ ShoesOnlineStore/otp/test/test_urls.py
@@ -1,33 +1,17 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
-from ..models import OtpRequest
+import unittest
+from django.urls import reverse, resolve
+from .. import views
 
 
-class OTPUrlTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
+class TestUrls(unittest.TestCase):
+    def test_request_otp_url(self):
+        url = reverse('otp_request')
+        self.assertEqual(resolve(url).func.view_class, views.RequestOTP)
 
-    def test_request_otp_endpoint(self):
-        data = {'phone_number': '+1234567890'}
-        response = self.client.post('/request_otp/', data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('otp_request_id', response.data)
+    def test_verify_otp_url(self):
+        url = reverse('otp_verify')
+        self.assertEqual(resolve(url).func.view_class, views.VerifyOtp)
 
-    def test_verify_otp_endpoint_valid(self):
-        otp_request = OtpRequest.objects.create(phone_number='+1234567890')
-        otp_request.generate_code()
-        otp_request.save()
-        data = {'phone_number': '+1234567890', 'code': otp_request.code}
-        response = self.client.post('/verify_otp/', data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('access_token', response.data)
-        self.assertIn('refresh_token', response.data)
 
-    def test_verify_otp_endpoint_invalid(self):
-        data = {'phone_number': '+1234567890', 'code': '1234'}  # Invalid code
-        response = self.client.post('/verify_otp/', data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['error'], 'Invalid data provided.')
-
-# Add more test cases as needed
+if __name__ == '__main__':
+    unittest.main()
