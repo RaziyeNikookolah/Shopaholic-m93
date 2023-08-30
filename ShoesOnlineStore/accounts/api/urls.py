98f840825185fc28c7cda6e4aaf45cd40974e1b3
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-08-30 08:50:50 +0330

    chore:add get_user_id_from_token url

--- ShoesOnlineStore/accounts/api/urls.py
+++ ShoesOnlineStore/accounts/api/urls.py
@@ -6,6 +6,8 @@ from accounts.api.views import router as accounts_router
 urlpatterns = [
     path('optain_pair_tokens/', views.TokenObtainPairView.as_view(),
          name="optain_pair_tokens"),
+    path('get_user_id_from_token/', views.get_user_id_in_token.as_view(),
+         name="get_user_id_from_token")
 
 ]
 
